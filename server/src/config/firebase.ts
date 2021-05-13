feaa2319482f12231e0e072715d5d673682580f9
tomiadebanjo <tomiadebanjo@gmail.com>
2021-05-13 12:07:12 +0100

    fix heroku private key parsing issue

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -1,12 +1,14 @@
 import admin from 'firebase-admin';
-import logger from '../utils/logger';
 import AppSecrets from '../utils/secrets';
 
 const config = {
   type: AppSecrets.FIREBASE_TYPE,
   projectId: AppSecrets.FIREBASE_PROJECT_ID,
   privateKeyId: AppSecrets.FIREBASE_PRIVATE_KEY_ID,
-  privateKey: JSON.parse(AppSecrets.FIREBASE_PRIVATE_KEY),
+  privateKey:
+    AppSecrets.FIREBASE_PRIVATE_KEY === '-'
+      ? AppSecrets.FIREBASE_PRIVATE_KEY
+      : JSON.parse(AppSecrets.FIREBASE_PRIVATE_KEY),
   clientEmail: AppSecrets.FIREBASE_CLIENT_EMAIL,
   clientId: AppSecrets.FIREBASE_CLIENT_ID,
   authUri: AppSecrets.FIREBASE_AUTH_URI,
@@ -14,8 +16,6 @@ const config = {
   auth_provider_x509_cert_url: AppSecrets.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: AppSecrets.FIREBASE_CLIENT_X509_CERT_URL,
 };
-
-// logger.info(config.privateKey);
 
 admin.initializeApp({
   credential: admin.credential.cert(config),
